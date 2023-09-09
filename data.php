60a96d63c34a02e4c8121aeda8ecc85186f3998b
Will-Alger <Algerw@icloud.com>
2023-09-09 13:04:58 -0400

    init

---
+++ data.php
@@ -0,0 +1,409 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Disclaimer
+|--------------------------------------------------------------------------
+|
+| Some of the dummy data for this project were generated using ChatGPT, an AI developed by OpenAI.
+| Some of the images associated with this project were created using Midjourney. 
+| Please note that these are used for demonstration and testing purposes only.
+|
+*/
+
+$data = array(
+    "1" => array(
+        "personal_info" => array(
+            "name" => "Will Alger",
+            "job_title" => "Full Stack Developer",
+            "email" => "algerw@icloud.com",
+            "phone_number" => "502-758-4615",
+            "summary" => "Adaptable CS undergraduate graduating in December 2023 with full-stack experience seeking full-time opportunity. Committed to continuous learning and growth in a dynamic and problem-solving environment.",
+            "img" => "assets/images/profile.jpg"
+        ),
+        "links" => array(
+            "linked_in" => "linkedin.com/in/williamalger/",
+            "github" => "github.com/will-alger"
+        ),
+        "education" => array(
+            array(
+                "university" => "Northern Kentucky University",
+                "university_timeline" => "2020 - 2023",
+                "degree" => "BS in Computer Science"
+            ),
+            // ... more degrees
+        ),
+        "skills" => array(
+            "main" => array(
+                "JavaScript" => "75%",
+                "Node.js" => "50%",
+                "Java" => "75%",
+                "C++" => "75%",
+                "Python" => "80%",
+                "HTML/CSS" => "80%",
+                "jQuery" => "95%",
+                "SQL" => "60%",
+            ),
+            "other" => array("Docker", "Git", "Jira", "Azure", "Postman"),
+        ),
+        "awards" => array(
+            array(
+                "title" => "Governors School for Entrepreneurs Scholarship",
+                "desc" => "Tuition award for completing GSE summer workshop program"
+            ),
+            array(
+                "title" => "Nightscout Foundation Scholarship",
+                "desc" => "Academic award towards students in STEM with diabetes"
+            )
+            // ... more awards
+        ),
+        "languages" => array(
+            "English", "Spanish (some)"
+        ),
+        "interests" => array(
+            "Running", "Cycling", "Lifting weights", "Cooking"
+        ),
+        "projects" => array(
+            array(
+                "title" => "Search Engine for Full-Stack Wiki Application",
+                "desc" => "
+					- Implemented an inverted-index based search engine for a full-stack wiki app utilizing Python, Flask, 
+					and SQLite with real-time indexing to ensure up to date search results.",
+                "img" => "assets\images\search.jpg",
+                "link" => "",
+            ),
+            array(
+                "title" => "Linear Feedback Shift Register: Visual Tool ",
+                "desc" => "
+					- Developed a dynamic LFSR visualizer on GitHub Pages using JavaScript, jQuery, Bootstrap, HTML & CSS
+					- Empowers cryptology students to deepen understanding of digital encryption.
+					- Visualizes a core concept in action for better learning experience.
+				",
+                "img" => "assets\images\LFSR.png",
+                "link" => "https://github.com/Will-Alger/LFSR",
+            )
+            // ... more projects
+        ),
+        "resume_items" => array(
+            array(
+                'position' => 'Software Engineer (intern)',
+                'company' => 'Kroger Technology',
+                'time' => 'May 2023 - Present',
+                'desc' => 'Developing and maintaining Node.js applications as part of an authentication and authorization team.',
+                'achievements' => array(),
+                'technologies' => array(
+                    'JavaScript', 'Node.js', 'jQuery', 'HTML/SASS', 'Azure AD B2C'
+                ),
+            ),
+            array(
+                'position' => 'Undergraduate Teaching Assistant',
+                'company' => 'Northern Kentucky University',
+                'time' => 'Aug 2022 -  Dec 2022',
+                'desc' => 'Teaching assistant for CSC 425 Advanced Programming Methods class',
+                'achievements' => array(
+                    'Supported 30+ students in an upper-level computer science course in C++.',
+                    'Developed and led interactive class workshops and live coding sessions, breaking down complex programming concepts in C++ into simplified, digestible explanations.',
+                ),
+                'technologies' => array(
+                    'C++'
+                ),
+            ),
+            array(
+                'position' => 'Software Engineer (intern)',
+                'company' => 'Kroger Technology',
+                'time' => 'May 2022 - Aug 2022',
+                'desc' => 'Front-End developement of dashboards and associate technology',
+                'achievements' => array(
+                    'Implemented a gamification leaderboard widget in Typescript, React, and Redux aimed at boosting employee productivity (piloted in multiple Kroger stores).',
+                ),
+                'technologies' => array(
+                    'React/Redux', 'TypeScript',  'React', 'HTML/SASS'
+                ),
+            )
+            // ... more resume items
+        )
+    ),
+    "2" => array(
+        "personal_info" => array(
+            "name" => "Billy Bob",
+            "job_title" => "Senior Software Engineer",
+            "email" => "billybob@hillbillycode.com",
+            "phone_number" => "+1 800 555 1234",
+            "summary" => "Born and raised in the Appalachian mountains, I found my passion for coding while trying to automate my moonshine production. Specialized in PHP, JavaScript, and Python, with a knack for solving complex problems and a love for clean, efficient code. Y'all won't find a more dedicated programmer!",
+            "img" => "assets/images/profile2.png"
+        ),
+        "links" => array(
+            "linked_in" => "https://www.linkedin.com/in/billybob",
+            "github" => "https://github.com/BillyBobTheCoder"
+        ),
+        "education" => array(
+            array(
+                "university" => "Appalachian State University",
+                "university_timeline" => "2020 - 2024",
+                "degree" => "BS in Computer Science",
+            ),
+        ),
+        "skills" => array(
+            "main" => array(
+                "JavaScript" => "85%",
+                "Tractor Programming" => "95%",
+                "Chickens/Roosters Logistics" => "90%",
+                "Squirrel++" => "80%",
+                "Python Wrangling" => "75%",
+                "HTML/CSS (Hay Tossing/Mule Cart Steering)" => "70%",
+                "jQuery (not confused with Dairy Queen)" => "65%",
+                "SQL (Squirrel Query Language)" => "60%",
+            ),
+            "other" => array("Moonshine Brewing - 100%", "Fiddle Playing - 90%", "Mud Sports - 85%", "Yodeling - 80%", "Barbecue Grilling - 75%"),
+        ),
+        "awards" => array(
+            array(
+                "title" => "Best Hay Bale Stack Overflow Solution",
+                "desc" => "Awarded for an innovative algorithm that optimized hay bale stackage, surprisingly using JavaScript's event loop."
+            ),
+            array(
+                "title" => "Fastest Tractor-Driven Coding Sprint",
+                "desc" => "Held the record for most lines of C++ code written while driving a tractor in an open field."
+            )
+        ),
+        "languages" => array(
+            "English",
+        ),
+        "interests" => array(
+            "Squirrel Hunting", "Mud Racing", "Barefoot Coding", "Whittling Keyboards"
+        ),
+        "projects" => array(
+            array(
+                "title" => "Hillbilly E-Commerce",
+                "desc" => "A revolutionary e-commerce platform that caters specifically to the hillbilly market. Built entirely with twigs and HTML.",
+                "img" => "assets\images\hillbilly_ecommerce.png",
+                "link" => "https://hillbilly-ecommerce.com",
+            ),
+            array(
+                "title" => "Squirrelly Social Media",
+                "desc" => "A social media network designed for squirrels and their adoring fans. It's nuts!",
+                "img" => "assets\images\squirrelly_social.png",
+                "link" => "https://squirrelly-social.com",
+            )
+        ),
+        "resume_items" => array(
+            array(
+                'position' => 'Lead Troubleshooting Extraordinaire',
+                'company' => 'HillBilly\'s Software Shack',
+                'time' => '2015 - Present',
+                'desc' => 'Responsible for keeping the peace between humans and machines.',
+                'achievements' => array(
+                    'Pioneered the use of duck tape in software repair.',
+                    'Turned coffee into code on a daily basis.',
+                    'Managed to keep all my hair despite numerous coding frustrations.'
+                ),
+                'technologies' => array(
+                    'Java', 'PHP', 'MySql', 'Node.js', 'Python'
+                ),
+            ),
+            array(
+                'position' => 'Junior Code Wrangler',
+                'company' => 'Bobby Joe\'s Programming Palace',
+                'time' => '2013 - 2015',
+                'desc' => 'Wrestled bugs and wrangled code into submission.',
+                'achievements' => array(
+                    'Won the annual "Fastest Coder in the South" competition.',
+                    'Never once spilled BBQ sauce on a keyboard (a noteworthy achievement in these parts).'
+                ),
+                'technologies' => array(
+                    'Python'
+                ),
+            ),
+            array(
+                'position' => 'Intern',
+                'company' => 'Cousin Jim\'s Javascript Jamboree',
+                'time' => '2012 - 2013',
+                'desc' => 'Learned the ropes and fetched a lot of coffee.',
+                'achievements' => array(
+                    'Successfully revived a dead computer with nothing but a can of soda and sheer determination.',
+                ),
+                'technologies' => array(
+                    'HTML',  'CSS', 'JavaScript', 'jQuery'
+                ),
+            )
+        )
+    ),
+    "3" => array(
+        "personal_info" => array(
+            "name" => "Ogg",
+            "job_title" => "Caveman Software Engineer",
+            "email" => "ogg@cave.io",
+            "phone_number" => "+1234567890",
+            "summary" => "Innovative Caveman Software Engineer with over 100,000 years of experience in software development. Specializes in rock-based programming and fire-driven technologies.",
+            "img" => "assets/images/profile3.png"
+        ),
+        "links" => array(
+            "linked_in" => "linkedin.com/in/ogg",
+            "github" => "github.com/ogg"
+        ),
+        "education" => array(
+            array(
+                "university" => "Stonehenge University",
+                "university_timeline" => "99,990 BC - 99,980 BC",
+                "degree" => "Bachelor of Rock Science",
+            ),
+            array(
+                "university" => "Pangea Institute of Technology",
+                "university_timeline" => "99,979 BC - 99,975 BC",
+                "degree" => "Ph.D. in Flintknapping Computer Science",
+            ),
+        ),
+        "skills" => array(
+            "main" => array(
+                "JavaScript" => "100%",
+                "Node.js" => "95%",
+                "Java" => "85%",
+                "C++" => "90%",
+                "Python" => "95%",
+                "HTML/CSS" => "100%",
+                "jQuery" => "80%",
+                "SQL" => "90%"
+            ),
+            "other" => array("Firebug", "Webpack", "MongoDB", "AWS", "Docker"),
+        ),
+        "awards" => array(
+            array(
+                "title" => "Best Rock App of 99,980 BC",
+                "desc" => "Developed a rock app that dramatically improved efficiency in hunting"
+            ),
+            array(
+                "title" => "Most Innovative Use of Fire",
+                "desc" => "Optimized the use of fire in our everyday tasks"
+            )
+        ),
+        "languages" => array(
+            "Caveman speak", "English"
+        ),
+        "interests" => array(
+            "Rock collection", "Fire making", "Cave painting", "Hunting"
+        ),
+        "projects" => array(
+            array(
+                "title" => "RockFolio",
+                "desc" => "A portfolio tracker for all your rock investments",
+                "img" => "assets/images/rockfolio.png",
+                "link" => "cave.io/rockfolio",
+            ),
+            array(
+                "title" => "FireHub",
+                "desc" => "A social network connecting all fire enthusiasts",
+                "img" => "assets/images/firehub.png",
+                "link" => "cave.io/firehub",
+            )
+        ),
+        "resume_items" => array(
+            array(
+                'position' => 'Senior Rock Developer',
+                'company' => 'Cave Innovations',
+                'time' => '99,995 BC - Present',
+                'desc' => 'Develop innovative rock solutions for modern cave problems',
+                'achievements' => array('Spearheaded a project to optimize rock usage', 'Improved rock-tool conversion rate by 30%'),
+                'technologies' => array(
+                    'JavaScript', 'Python', 'Node.js', 'Java', 'C++'
+                ),
+            ),
+            array(
+                'position' => 'Junior Fire Engineer',
+                'company' => 'FireTech',
+                'time' => '99,998 BC - 99,995 BC',
+                'desc' => 'Maintained and optimized fire technology',
+                'achievements' => array(
+                    'Developed a new way to start fires that decreased fire-starting time by 50%',
+                    'Invented a method to keep fires burning longer with less fuel',
+                    'Awarded Most Innovative Use of Fire in 99,996 BC'
+                ),
+                'technologies' => array(
+                    'FireScript'
+                ),
+            ),
+            array(
+                'position' => 'Intern',
+                'company' => 'Stone Tools Corporation',
+                'time' => '99,999 BC - 99,998 BC',
+                'desc' => 'Assisted in production and optimization of stone tools',
+                'achievements' => array(
+                    'Recommended improvements in tool design that increased efficiency by 20%',
+                ),
+                'technologies' => array(
+                    'RockML',  'Chisel.js', 'Sledgehammer++', 'PebbleDB'
+                ),
+            )
+        )
+    ),
+    // ... other people
+);
+
+/* 
+    TEMPLATE for person:
+
+        "1" => array(
+            "personal_info" => array(
+                "name" => "",
+                "job_title" => "",
+                "email" => "",
+                "phone_number" => "",
+                "summary" => "",
+                "img" => ""
+            ),
+            "links" => array(
+                "linked_in" => "",
+                "github" => ""
+            ),
+            "education" => array(
+                array(
+                    "university" => "",
+                    "university_timeline" => "",
+                    "degree" => ""
+                )
+                // ... more degrees
+            ),
+            "skills" => array(
+                "main" => array(
+                    "JavaScript" => "",
+                    "Node.js" => "",
+                    ...
+                    
+                ),
+                "other" => array(),
+            ),
+            "awards" => array(
+                array(
+                    "title" => "",
+                    "desc" => ""
+                )
+            ),
+            "languages" => array(),
+            "interests" => array(),
+            "projects" => array(
+                array(
+                    "title" => "",
+                    "desc" => "",
+                    "img" => "",
+                    "link" => "",
+                )
+            ),
+            "resume_items" => array(
+                array(
+                    'position' => '',
+                    'company' => '',
+                    'time' => '',
+                    'desc' => '',
+                    'achievements' => array(),
+                    'technologies' => array(),
+                )
+            )
+        )
+
+
+
+
+
+
+
+
+*/
---
+++ detail.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php
+require_once 'data.php';
+$person = $data[$_GET['id']]
+?>
+
+<head>
+	<title><?= $person['personal_info']['name'] . "'s Resume"; ?></title>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Your name's resume">
+	<meta name="author" content="Your name">
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+	<!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+
+	<!-- Theme CSS -->
+	<link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
+
+
+</head>
+
+<body>
+	<article class="resume-wrapper text-center position-relative">
+		<div class="mb-4"><a href="index.php" class="btn btn-primary">Back to index</a></div>
+		<div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+			<header class="resume-header pt-4 pt-md-0">
+				<div class="row">
+					<div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+						<img class="picture" src="<?= $person['personal_info']['img']  ?>" alt="">
+					</div><!--//col-->
+					<div class="col">
+						<div class="row p-4 justify-content-center justify-content-md-between">
+							<div class="primary-info col-auto">
+								<h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"></h1>
+								<div class="title mb-3"><?= $person['personal_info']['job_title'] ?></div>
+								<ul class="list-unstyled">
+									<li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?= $person['personal_info']['email'] ?></a></li>
+									<li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?= $person['personal_info']['phone_number'] ?></a></li>
+								</ul>
+							</div><!--//primary-info-->
+							<div class="secondary-info col-auto mt-2">
+								<ul class="resume-social list-unstyled">
+									<li class="mb-3"><a class="text-link" href="<?= "https://" . $person['links']['linked_in'] ?>"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?= $person['links']['linked_in'] ?></a></li>
+									<li class="mb-3"><a class="text-link" href="<?= "https://" . $person['links']['github'] ?>"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?= $person['links']['github'] ?></a></li>
+								</ul>
+							</div><!--//secondary-info-->
+						</div><!--//row-->
+
+					</div><!--//col-->
+				</div><!--//row-->
+			</header>
+			<div class="resume-body p-5">
+				<section class="resume-section summary-section mb-5">
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Summary</h2>
+					<div class="resume-section-content">
+						<p class="mb-0"><?= $person['personal_info']['summary'] ?></p>
+					</div>
+				</section><!--//summary-section-->
+				<div class="row">
+					<div class="col-lg-9">
+						<section class="resume-section experience-section mb-5">
+							<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Work Experience</h2>
+							<div class="resume-section-content">
+								<div class="resume-timeline position-relative">
+									<!-- Loop through each resume item and output its data -->
+									<?php foreach ($person['resume_items'] as $item) : ?>
+										<article class="resume-timeline-item position-relative pb-5">
+											<div class="resume-timeline-item-header mb-2">
+												<div class="d-flex flex-column flex-md-row">
+													<h3 class="resume-position-title font-weight-bold mb-1"><?= $item['position'] ?></h3>
+													<div class="resume-company-name ms-auto"><?= $item['company'] ?></div>
+												</div><!--//row-->
+												<div class="resume-position-time"><?= $item['time'] ?></div>
+											</div><!--//resume-timeline-item-header-->
+											<div class="resume-timeline-item-desc">
+												<p><?= $item['desc'] ?></p>
+												<?php if (!empty($item['achievements'])) : ?>
+													<h4 class="resume-timeline-item-desc-heading font-weight-bold">Achievements:</h4>
+												<?php endif; ?>
+												<ul>
+													<?php foreach ($item['achievements'] as $achievement) : ?>
+														<li><?= $achievement ?></li>
+													<?php endforeach; ?>
+												</ul>
+												<?php if (!empty($item['technologies'])) : ?>
+													<h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+												<?php endif; ?>
+
+												<ul class="list-inline">
+													<?php foreach ($item['technologies'] as $technology) : ?>
+														<li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?= $technology ?></span></li>
+													<?php endforeach; ?>
+												</ul>
+											</div>
+										</article>
+									<?php endforeach; ?>
+								</div>
+							</div>
+						</section>
+					</div>
+					<div class="col-lg-3">
+						<section class="resume-section skills-section mb-5">
+							<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Skills &amp; Tools</h2>
+							<div class="resume-section-content">
+								<div class="resume-skill-item">
+									<ul class="list-unstyled mb-4">
+										<?php foreach ($person['skills']['main'] as $skill => $mastery) : ?>
+											<li class="mb-2">
+												<div class="resume-skill-name"><?= $skill ?></div>
+												<div class="progress resume-progress">
+													<div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: <?= $mastery ?>" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+												</div>
+											</li>
+										<?php endforeach; ?>
+									</ul>
+								</div><!--//resume-skill-item-->
+								<div class="resume-skill-item">
+									<h4 class="resume-skills-cat font-weight-bold">Others</h4>
+									<ul class="list-inline">
+										<?php foreach ($person['skills']['other'] as $skill) : ?>
+											<li class="list-inline-item"><span class="badge badge-light"><?= $skill; ?></span></li>
+										<?php endforeach; ?>
+									</ul>
+								</div>
+							</div><!--resume-section-content-->
+						</section><!--//skills-section-->
+						<section class="resume-section education-section mb-5">
+							<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
+							<div class="resume-section-content">
+								<ul class="list-unstyled">
+									<?php foreach ($person['education'] as $education) : ?>
+										<li class="mb-2">
+											<div class="resume-degree font-weight-bold"><?= $education['degree'] ?></div>
+											<div class="resume-degree-org"><?= $education['university'] ?></div>
+											<div class="resume-degree-time"><?= $education['university_timeline'] ?></div>
+										</li>
+									<?php endforeach; ?>
+								</ul>
+							</div>
+						</section><!--//education-section-->
+						<section class="resume-section reference-section mb-5">
+							<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Awards</h2>
+							<div class="resume-section-content">
+								<ul class="list-unstyled resume-awards-list">
+									<?php foreach ($person['awards'] as $award) : ?>
+										<li class="mb-2 ps-4 position-relative">
+											<i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+											<div class="resume-award-name"><?= $award["title"]; ?></div>
+											<div class="resume-award-desc"><?= $award["desc"]; ?></div>
+										</li>
+									<?php endforeach; ?>
+								</ul>
+							</div>
+						</section><!--//interests-section-->
+						<section class="resume-section language-section mb-5">
+							<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
+							<div class="resume-section-content">
+								<ul class="list-unstyled resume-lang-list">
+									<?php foreach ($person['languages'] as $language) : ?>
+										<li class="mb-2">
+											<span class="resume-lang-name font-weight-bold"><?= $language; ?></span>
+										</li>
+									<?php endforeach; ?>
+								</ul>
+							</div>
+						</section><!--//language-section-->
+						<section class="resume-section interests-section mb-5">
+							<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
+							<div class="resume-section-content">
+								<ul class="list-unstyled">
+									<?php foreach ($person['interests'] as $interest) : ?>
+										<li class="mb-1"><?= $interest; ?></li>
+									<?php endforeach; ?>
+								</ul>
+							</div>
+						</section>
+					</div>
+				</div>
+				<section class="resume-section experience-section mb-5">
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Projects</h2>
+					<div class="row mt-4 justify-content-center">
+						<?php foreach ($person['projects'] as $project) : ?>
+							<div class="col-md-6 mb-4 d-flex align-items-stretch">
+								<div class="card w-100">
+									<img src="<?= $project['img']; ?>" alt="<?= $project['title']; ?>" class="card-img-top">
+									<div class="card-body">
+										<h5 class="card-title"><?= $project['title']; ?></h5>
+										<p class="card-text"><?= $project['desc']; ?></p>
+										<a href="<?= $project['link']; ?>" class="btn btn-outline-primary">Go to link</a>
+									</div>
+								</div>
+							</div>
+						<?php endforeach; ?>
+					</div>
+			</div>
+			</section>
+		</div>
+		</div>
+	</article>
+	<footer class="footer text-center pt-2 pb-5">
+		<!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+		<small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by <?= $name ?> </small>
+	</footer>
+</body>
+
+</html>
